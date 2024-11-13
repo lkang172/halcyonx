de892e816d3e0f2696eac59c636a64e38d0ea2a0
Lucas Kang <kangzundao@gmail.com>
2024-11-12 23:36:08 -0500

    created record

--- src/main/java/com/github/lkang172/halcyonx/HalcyonxApplication.java
+++ src/main/java/com/github/lkang172/halcyonx/HalcyonxApplication.java
@@ -1,13 +1,36 @@
 package com.github.lkang172.halcyonx;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.CommandLineRunner;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import run.Location;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import run.Run;
 
 @SpringBootApplication
 public class HalcyonxApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(HalcyonxApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(HalcyonxApplication.class, args);
+		log.info("Application started running successfully");
+
+	}
+
+	@Bean
+	CommandLineRunner runner() {
+		return args -> {
+			Run run = new Run(1, "First run", LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS), 5,
+					Location.OUTDOOR);
+			log.info("Run: " + run);
+		};
 	}
 
 }
---
+++ src/main/java/run/Run.java
@@ -0,0 +1,8 @@
+package run;
+
+import java.time.LocalDateTime;
+
+public record Run(Integer id, String title, LocalDateTime startedOn, LocalDateTime completedOn, Integer miles,
+        Location location) {
+
+}
