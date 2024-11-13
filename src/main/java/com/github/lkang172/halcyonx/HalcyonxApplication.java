be545b59146fe6a9e42e618d2a2074f931900474
Lucas Kang <kangzundao@gmail.com>
2024-11-12 22:54:11 -0500

    source code

---
+++ src/main/java/com/github/lkang172/halcyonx/HalcyonxApplication.java
@@ -0,0 +1,13 @@
+package com.github.lkang172.halcyonx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HalcyonxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HalcyonxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/lkang172/halcyonx/HalcyonxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lkang172.halcyonx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HalcyonxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
