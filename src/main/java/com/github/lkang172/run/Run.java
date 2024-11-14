ccc2089094caa21a27a38908040c5af9ad033fb4
Lucas Kang <kangzundao@gmail.com>
2024-11-13 23:50:53 -0500

    add validation constraints

--- src/main/java/com/github/lkang172/run/Run.java
+++ src/main/java/com/github/lkang172/run/Run.java
@@ -2,7 +2,10 @@ package com.github.lkang172.run;
 
 import java.time.LocalDateTime;
 
-public record Run(Integer id, String title, LocalDateTime startedOn, LocalDateTime completedOn, Integer miles,
-        Location location) {
+import jakarta.validation.constraints.*;
+
+public record Run(Integer id, @NotEmpty String title, LocalDateTime startedOn, LocalDateTime completedOn,
+                @Positive Integer miles,
+                Location location) {
 
 }
